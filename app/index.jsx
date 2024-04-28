747c36581278d4a23cac575cb470a95d7cb1fb55
elvoceouma <elviceoumaonyango@gmail.com>
2024-04-28 14:29:45 +0300

    layout added

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Entry Point! Working</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Profile
+      </Link>
     </View>
   );
 }
